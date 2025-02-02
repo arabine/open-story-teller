09637be9987bd6c580d09d46c7e6a684898ef252
anthony@rabine.fr <Anthony>
2025-02-02 20:38:25 +0100

    fix ci and nsis package generation

--- story-editor/delivery/linux_03_docker_launch_package.sh
+++ story-editor/delivery/linux_03_docker_launch_package.sh
@@ -1,3 +1,3 @@
 
-docker run --privileged -v $(pwd)/..:/workspace cpp-dev-linux /bin/bash -c "cd /workspace/story-editor && ./build_appimage.sh"
+docker run --privileged -v $(pwd)/../..:/workspace cpp-dev-linux /bin/bash -c "cd /workspace/story-editor && ./build_appimage.sh"
 
